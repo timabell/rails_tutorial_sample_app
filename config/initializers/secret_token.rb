a5442b450634b0250c9ea0a8f7437d037f6f241c
Tim Abell <tim@timwise.co.uk>
2013-05-17 13:51:45 +0100

    blank rails app, skip-test-uni, ch 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '9952d0c3e7ecbb92fce92e939bfefb71b618aa9fde7485b39ad9b7ebe3bb6b8c7d0ccf8e5b1af6af53510ce88526931ee5be95e8923834e635de5d035c68ec91'
