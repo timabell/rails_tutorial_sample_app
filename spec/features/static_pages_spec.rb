03d527c75f2beb7f46c1225a32c637741a8379ec
Tim Abell <tim@timwise.co.uk>
2013-05-22 16:39:20 +0100

    refactored tests, ch 3.5, ex 2

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -8,7 +11,7 @@ describe "StaticPages" do
     end
     it 'should have the right title' do
       visit '/static_pages/home'
-      expect(page).to have_title "Ruby on Rails Tutorial Sample App | Home"
+      expect(page).to have_title "#{base_title} | Home"
     end
   end
   describe "Help page" do
@@ -18,7 +21,7 @@ describe "StaticPages" do
     end
      it 'should have the right title' do
       visit '/static_pages/help'
-      expect(page).to have_title "Ruby on Rails Tutorial Sample App | Help"
+      expect(page).to have_title "#{base_title} | Help"
     end
   end
   describe "About Us page" do
@@ -28,7 +31,7 @@ describe "StaticPages" do
     end
       it 'should have the right title' do
       visit '/static_pages/about'
-      expect(page).to have_title "Ruby on Rails Tutorial Sample App | About"
+      expect(page).to have_title "#{base_title} | About"
     end
   end
 end
