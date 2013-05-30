f8cec1db6dca7b53e70866c68633ec463c7a34e2
Tim Abell <tim@timwise.co.uk>
2013-05-30 23:16:24 +0100

    factored out visit root_path, ch 5.3.4

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -5,16 +5,15 @@ describe "StaticPages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
+    before { visit root_path }
+
     it "should have the content 'Sample App'" do
-      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
     it 'should have the right title' do
-      visit root_path
       expect(page).to have_title "#{base_title}"
     end
     it 'shouldn\'t have a per-page title' do
-      visit root_path
       expect(page).not_to have_title('|')
     end
   end
