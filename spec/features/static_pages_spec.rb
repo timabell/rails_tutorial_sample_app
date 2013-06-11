1942c3482ad60d07121c7f33ace2f563c19b88da
Tim Abell <tim@timwise.co.uk>
2013-06-11 01:39:43 +0100

    Refactored tests as per listing 5.27

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,47 +2,34 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
 
   describe "Home page" do
     before { visit root_path }
-    subject { page }
 
     it { should have_selector('h1', :text => 'Sample App') }
-    it { should have_title("#{base_title}") }
+    it { should have_title(full_title('')) }
     it { should_not have_title('|') }
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit help_path
-      page.should have_selector('h1', :text => 'Help')
-    end
-     it 'should have the right title' do
-      visit help_path
-      expect(page).to have_title "#{base_title} | Help"
-    end
+    before { visit help_path }
+
+    it { should have_selector('h1', :text => 'Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About Us page" do
-    it "should have the content 'About Us'" do
-      visit about_path
-      page.should have_selector('h1', :text => 'About Us')
-    end
-      it 'should have the right title' do
-      visit about_path
-      expect(page).to have_title "#{base_title} | About"
-    end
+    before { visit about_path }
+
+    it { should have_selector('h1', :text => 'About') }
+    it { should have_title(full_title('About')) }
   end
 
   describe "Contact page" do
-    it "should have the content 'Contact'" do
-      visit contact_path
-      expect(page).to have_content('Contact')
-    end
-    it "should have the title 'Contact'" do
-      visit contact_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
-    end
+    before { visit contact_path }
+
+    it { should have_selector('h1', :text => 'Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
